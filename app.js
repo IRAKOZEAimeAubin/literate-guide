99d00ad7d1e27c5e09beacd18252a59754c9db8c
Irakoze Aime Aubin <irakozeaimeaubin@gmail.com>
2022-08-29 15:48:26 +0200

    commit #1

---
+++ app.js
@@ -0,0 +1,25 @@
+// GETTING THE DATA
+async function getRates () {
+    document.getElementById( 'result' ).innerHTML = '';
+    try {
+        const selected = document.getElementById( 'currency' );
+        const currency = selected.options[ selected.selectedIndex ].text;
+        const res = await axios.get( `https://open.er-api.com/v6/latest/${currency}` );
+        const data = res.data;
+        console.log( data.time_last_update_utc );
+        for ( const keys in data.rates ) {
+            document.getElementById( 'result' ).innerHTML += `
+            <tr>
+                <td>+${keys}</td>
+                <td>${data.rates[keys]}</td>
+                <td>${data.time_last_update_utc}</td>
+                <td>${data.time_next_update_utc}</td>
+            </tr>`
+        }
+    } catch ( error ) {
+        console.log( error );
+    }
+}
+
+document.getElementById( 'currency' ).addEventListener( 'change', getRates );
+window.addEventListener( 'load', getRates );
